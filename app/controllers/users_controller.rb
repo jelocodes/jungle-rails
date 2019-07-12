8e29c60c71332d6ee1aca058a886a3568f644712
jelocodes <michaelangelo.yambao@gmail.com>
2019-07-12 00:56:06 +0000

    standardize @user instance function notation

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -4,14 +4,20 @@ class UsersController < ApplicationController
   end
 
   def create
-    @user = User.new()
+    @user = User.new(user_params)
     if @user.save
-        session[:user_id] = user.id
+        session[:user_id] = @user.id
         flash[:success] = 'Successfully created user!'
         redirect_to '/'
     else
       flash[:warning] = 'Invalid email or password. Please try again.'
       redirect_to '/users/new'
     end
+  end
+
+  private
+
+  def user_params
+    params.require(:user).permit(:name, :email, :password, :password_confirmation)
   end
 end
