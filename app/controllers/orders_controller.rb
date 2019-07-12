991499b19fd067f63091c1ac5fca436084f8d4ec
jelocodes <michaelangelo.yambao@gmail.com>
2019-07-12 02:47:12 +0000

    change config to send mail in development instantly instead of in queue

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -11,7 +11,7 @@ class OrdersController < ApplicationController
     if order.valid?
       empty_cart!
       redirect_to order, notice: 'Your Order has been placed.'
-      OrderMailer.order_email(order, User.find(session[:user_id]).email).deliver_later
+      OrderMailer.order_email(order, User.find(session[:user_id]).email).deliver_now
     else
       redirect_to cart_path, flash: { error: order.errors.full_messages.first }
     end
