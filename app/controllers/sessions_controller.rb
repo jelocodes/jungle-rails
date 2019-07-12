9e835d3ae14e71cb54f834e9aa9b947103221205
jelocodes <michaelangelo.yambao@gmail.com>
2019-07-12 00:59:26 +0000

    fix quotation mark formatting

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -5,16 +5,16 @@ def create
  @user = User.find_by(email: params[:email])
  if @user && @user.authenticate(params[:password])
   session[:user_id] = @user.id
-  flash[:success] = ‘Successfully Logged In!’
-  redirect_to ‘/contacts’
+  flash[:success] = 'Successfully Logged In!'
+  redirect_to '/'
  else
-  flash[:warning] = “Invalid Username or Password”
-  redirect_to ‘/login’
+  flash[:warning] = "Invalid Username or Password"
+  redirect_to '/login'
  end
 end
  def destroy
   session[:user_id] = nil
-  flash[:success] = “Successfully Logged Out!”
-  redirect_to ‘/login’
+  flash[:success] = "Successfully Logged Out!"
+  redirect_to '/login'
  end
 end
